e2d9ba0c5a01c8df84b1dbc666ca3f99b723b0d0
GiselleSerate <giselle.serate@gmail.com>
2019-03-06 00:21:44 -0800

    Comment pointTracking files

--- nodes/pointTracking.py
+++ nodes/pointTracking.py
@@ -6,97 +6,7 @@ import math
 from geometry_msgs.msg import Twist, Vector3
 import tf
 
-# kp = 3.0
-# ka = 0.0
-# kb = -4.0
-
-# kp = 1.5
-# ka = 0.0
-# kb = -2.0
-
-# kp = 1
-# ka = 1
-# kb = -2.0
-
-# # Strong stability control (assume kb < 0)
-# kp = 0.5
-# ka = 1
-# kb = -0.2
-# # Slow, but it got there. 
-
-# # Inc kp, ka. 
-# kp = 0.7
-# ka = 1.2
-# kb = -0.2
-# # too fast forward
-
-# # dec kp, inc ka. 
-# kp = 0.5
-# ka = 1.5
-# kb = -0.2
-# # doesn't seem to be stable
-
-# # Strong stability control, assuming slides correct
-# kp = 0.5
-# ka = -0.5
-# kb = -0.2
-# # wanders off towards the right instead of going towards goal
-
-# # Normal stability control as suggested in the slides
-# kp = 3
-# ka = 7
-# kb = -1.5
-# # straight line
-
-# # Normal stability control
-# kp = 0.5
-# ka = 1
-# kb = -1.5
-# # 1m to the left but not fwd
-
-# # Normal stability control
-# kp = 0.5
-# ka = 1
-# kb = -3
-# # further backwards than previous
-
-# # Retrying
-# kp = 1
-# ka = 1
-# kb = -2.0
-# # escaping, bye
-
-# # Retrying with smaller kp
-# kp = 0.5
-# ka = 1
-# kb = -2.0
-# # not far enough +x in 1,1,0; almost perf in 1,0,0
-
-# # Retrying with smaller ka
-# kp = 0.5
-# ka = 0.7
-# kb = -2.0
-# # overshooting
-
-# # Retrying with smaller kb
-# kp = 0.5
-# ka = 0.7
-# kb = -1.0
-# # Seem like good consts
-
-# # Retrying with larger kp
-# kp = 0.6
-# ka = 0.7
-# kb = -1.0
-# # 
-
-# # Retrying with bigger ka
-# kp = 0.6
-# ka = 0.9
-# kb = -1.0
-# # bad, go back
-
-# Roll back to good
+# P control constants
 kp = 0.5
 ka = 0.7
 kb = -1.0
@@ -112,23 +22,27 @@ def pointTracking():
     while not rospy.is_shutdown():
         try:
             now = rospy.Time.now()
+
+            # Get transform from tf
             listener.waitForTransform("/base_link", "/goal", now, rospy.Duration(4.0))
             (trans,rot) = listener.lookupTransform("/base_link", "/goal", now)
 
+            # Get P control distances and angles
             theta = tf.transformations.euler_from_quaternion(rot)[2]
-
             rho = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
             beta = -1 * math.atan2(trans[1], trans[0])
             alpha = -1 * beta - theta
 
+            # Calculate velocities
             angular = ka * alpha + kb * beta
             linear = kp * rho
 
+            # Stop driving when close enough to goal
             if rho < 0.02:
                 linear = .0
                 angular = .0
 
-
+            # Publish velocity to control.py
             cmd = Twist()
             cmd.linear.x = linear
             cmd.angular.z = angular
--- nodes/subGoal.py
+++ nodes/subGoal.py
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     broadcaster = tf.TransformBroadcaster()
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
+    	# Send hard-coded transform
         broadcaster.sendTransform((2,0,0), tf.transformations.quaternion_from_euler(.0, .0, 0), rospy.Time.now(), "/goal", "/odom")
         rate.sleep()
